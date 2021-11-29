b549d2f4e842bcfa92d01809b6e8bf2513e8a749
PeroStevenz <95230862+PeroStevenz@users.noreply.github.com>
2021-11-30 05:35:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `T có điều này muốn hỏi m, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 't biết m thích t phải không phải không ? :3',
+    desc: 'M không trả lời hoặc thoát ra, tức là m thích t đó nha :> ',
+    btnYes: 'Có, t thật sự thích m 💖💖💖',
+    btnNo: 'Mơ đi ',
+    question:'Nói cho t biết đi vì sao m thích t 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    reply: 'Tại vì m cute đó Ahiiiii 😊😊',
+    mess: 'T biết mà, t cũng thích m lắm 🥰. Love you 300.000 😘😘',
+    messDesc: ' :)) đùa đấy mai tặng cho cây kẹo mút nha :>',
     btnAccept: 'Ok lun 💖',
     messLink: 'https://m.me/PhungQuocDinh'
 }
